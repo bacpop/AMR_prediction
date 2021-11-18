7ecf74ab1ebf712eafc1731d89cd2beada63fbd7
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2021-11-18 16:18:46 +0000

    for loop passing by reference, added std::

--- amr_pred.cpp
+++ amr_pred.cpp
@@ -1,6 +1,6 @@
 #include "./Matrix.h"
 #include "./kseq.h"
-#include "./zlib.h" // try including -s KSEQ_INIT(gzFile, gzread) to web options??
+#include "./zlib.h" 
 #include <sdsl/suffix_arrays.hpp>
 #include<iostream>
 #include<iomanip>
@@ -15,18 +15,16 @@
 
 KSEQ_INIT(gzFile, gzread)
 
-using namespace std;
-//using namespace emscripten;
 
-void read_model(string antibiotic, vector<string>& unitigs, vector<double>& coefs)
+void read_model(const std::string& antibiotic, std::vector<std::string>& unitigs, std::vector<double>& coefs)
 {
-    ostringstream filename; //set filepath and open as istream
+    std::ostringstream filename; //set filepath and open as istream
     filename<<"files/model_coefficients/"<<antibiotic<<"_coefficients.txt";
-    ifstream ist {filename.str()};
+    std::ifstream ist {filename.str()};
     if(!ist) perror("Can't open model input file ");
 
-    string unitig;  
-    float coef;
+    std::string unitig;  
+    double coef;
     while (ist>>unitig>>coef)   //fill data in 2 vectors
     {   
         unitigs.push_back(unitig);
@@ -34,9 +32,9 @@ void read_model(string antibiotic, vector<string>& unitigs, vector<double>& coef
     }
 }
 
-sdsl::csa_wt<> create_index(string filepath) 
+sdsl::csa_wt<> create_index(std::string filepath) 
 { 
-    string reference_seq;
+    std::string reference_seq;
 
     // open the file handler
     gzFile fp = gzopen(filepath.c_str(), "r");
@@ -92,7 +90,7 @@ char complement(char n)
     return ' ';
 }
 
-string invert(string seq){
+std::string invert(std::string seq){
     
     transform(
         begin(seq),
@@ -103,10 +101,11 @@ string invert(string seq){
     return seq;
 }
 
-vector<double> lookup_unitigs(sdsl::csa_wt<> fm_index, vector<string> unitigs)
+std::vector<double> lookup_unitigs(const sdsl::csa_wt<>& fm_index, const std::vector<std::string>& unitigs)
 {
-    vector<double> pa; //vector to store presence/absence
-    for(string i:unitigs){
+    std::vector<double> pa; //vector to store presence/absence
+    pa.reserve(unitigs.size());
+    for(std::string i:unitigs){
         if (i=="Intercept"){pa.push_back(1.0);} //start each vector with 1 for the Intercept
         else{
             int c = count(fm_index,i);
@@ -124,28 +123,28 @@ vector<double> lookup_unitigs(sdsl::csa_wt<> fm_index, vector<string> unitigs)
 
 
 
-void make_prediction(string assembly_filename)
+void make_prediction(const std::string& assembly_filename)
 {
     auto start = std::chrono::steady_clock::now();
 
     sdsl::csa_wt<> fm_index = create_index(assembly_filename); // create fm-Index from .fasta file
 
-    vector<string> antibiotics ={"Penicillin","Chloramphenicol","Erythromycin","Tetracycline", "Trim_sulfa"};
+    std::vector<std::string> antibiotics ={"Penicillin","Chloramphenicol","Erythromycin","Tetracycline", "Trim_sulfa"};
 
-    for(string antibiotic:antibiotics){
+    for(std::string const &antibiotic:antibiotics){ 
 
-        vector<string> unitigs; //store data from model
-        vector<double> coefs;
+        std::vector<std::string> unitigs; //store data from model
+        std::vector<double> coefs;
         read_model(antibiotic, unitigs, coefs);  //fill vectors
         Numeric_lib::Matrix<double,1>coefs_mat{&coefs[0], int(coefs.size())}; // translate into Matrix
 
-        vector<double> pa = lookup_unitigs(fm_index, unitigs); // get vector of presence/absence of unitigs
+        std::vector<double> pa = lookup_unitigs(fm_index, unitigs); // get vector of presence/absence of unitigs
         Numeric_lib::Matrix<double,1>pa_mat{&pa[0], int(pa.size())}; // translate into Matrix for calculations
 
         double pred = Numeric_lib::dot_product(pa_mat,coefs_mat); // calculate dot product of both Matrices
         double prob =1/(1+exp(-pred)); // apply sigmoid function for probability of resistance
 
-        cout<<"Probability of resistance to "<<antibiotic<<": "<<prob<<'\n';
+        std::cout<<"Probability of resistance to "<<antibiotic<<": "<<prob<<'\n';
 
     }  
 
@@ -157,15 +156,15 @@ void make_prediction(string assembly_filename)
 
 int main()
 {
-    string id_file = "files/mass_assemblies_ids_short.txt";
-    ifstream ist {id_file};
+    std::string id_file = "files/mass_assemblies_ids_short.txt";
+    std::ifstream ist {id_file};
     if(!ist) perror("Can't open file with strain IDs");   
 
-    string id;
+    std::string id;
     while(ist>>id)
     {
-        string assembly_filename = "files/assemblies_mass_short/"+id+".fa";
-        cout<<'\n'<<assembly_filename<<'\n';
+        std::string assembly_filename = "files/assemblies_mass_short/"+id+".fa";
+        std::cout<<'\n'<<assembly_filename<<'\n';
         make_prediction(assembly_filename);
     }
     
