2858c08aaa56c370c9b9917a31fa4eb69dd85ab6
muppi1993 <clara.gronemeyer20@imperial.ac.uk>
2021-12-01 13:00:40 +0000

    class Model now in seperate header file

---
+++ Model.h
@@ -0,0 +1,38 @@
+#include<vector>
+#include<string>
+#include<fstream>
+#include<cmath>
+
+class Model{
+        std::vector<std::string> unitigs;
+        std::vector<double> coefs;
+    public:
+        std::vector<std::string> getUnitigs(){return unitigs;}
+        
+        void read_model(const std::string& antibiotic)
+        {
+            //std::ostringstream filename; //set filepath and open as istream
+            //filename<<"files/model_coefficients/"<<antibiotic<<"_coefficients.txt";
+            std::string filename= "files/model_coefficients/"+antibiotic+"_coefficients.txt"; // more or less efficient?? other alternative: .append()
+            std::ifstream ist {filename};
+            if(!ist) perror("Can't open model input file ");
+
+            std::string unitig;  
+            double coef;
+            while (ist>>unitig>>coef)   //fill data in 2 vectors
+            {   
+                unitigs.push_back(unitig);
+                coefs.push_back(coef);
+            }
+        }
+
+        double get_prob(const Numeric_lib::Matrix<double,1>& pa_mat)
+        {
+            Numeric_lib::Matrix<double,1>coefs_mat{&coefs[0], int(coefs.size())}; // translate coefs vector into Matrix
+            double pred = Numeric_lib::dot_product(pa_mat,coefs_mat); // calculate dot product
+            double prob =1/(1+exp(-pred)); // apply sigmoid function for probability of resistance
+            return prob;
+
+        }
+
+};
--- amr_pred.cpp
+++ amr_pred.cpp
@@ -1,4 +1,5 @@
 #include "./Matrix.h"
+#include "./Model.h"
 #include "./kseq.h"
 #include <zlib.h>
 #include <sdsl/suffix_arrays.hpp>
@@ -18,48 +19,6 @@ using json = nlohmann::json;
 
 
 KSEQ_INIT(gzFile, gzread)
-
-class Model{
-        std::vector<std::string> unitigs;
-        std::vector<double> coefs;
-    public:
-        std::vector<std::string> getUnitigs();
-        void read_model(const std::string&);
-        double get_prob(const Numeric_lib::Matrix<double,1>&);
-
-};
-
-std::vector<std::string> Model::getUnitigs(){
-    return unitigs;
-}
-
-
-void Model::read_model(const std::string& antibiotic)
-{
-    //std::ostringstream filename; //set filepath and open as istream
-    //filename<<"files/model_coefficients/"<<antibiotic<<"_coefficients.txt";
-    std::string filename= "files/model_coefficients/"+antibiotic+"_coefficients.txt"; // more or less efficient?? other alternative: .append()
-    std::ifstream ist {filename};
-    if(!ist) perror("Can't open model input file ");
-
-    std::string unitig;  
-    double coef;
-    while (ist>>unitig>>coef)   //fill data in 2 vectors
-    {   
-        unitigs.push_back(unitig);
-        coefs.push_back(coef);
-    }
-}
-
-double Model::get_prob(const Numeric_lib::Matrix<double,1>& pa_mat)
-{
-    Numeric_lib::Matrix<double,1>coefs_mat{&coefs[0], int(coefs.size())}; // translate coefs vector into Matrix
-    double pred = Numeric_lib::dot_product(pa_mat,coefs_mat); // calculate dot product
-    double prob =1/(1+exp(-pred)); // apply sigmoid function for probability of resistance
-    return prob;
-
-}
-    
 
 sdsl::csa_wt<> create_index(std::string filepath) 
 { 
